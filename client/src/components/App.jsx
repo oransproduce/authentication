49be839198f079da90ee6efe7c2beb35904ec0f5
oransproduce <remy.orans@gmail.com>
2021-05-11 23:34:16 -0700

    React router with no authentication implemented

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,7 +1,29 @@
 import React from 'react';
+import {
+  BrowserRouter, Link, Route, Switch,
+} from 'react-router-dom';
+import Public from './Public';
+import Protected from './Protected';
 
 export default function App() {
   return (
-    <h1>React is rendering</h1>
+    <BrowserRouter>
+      <ul>
+        <li>
+          <Link to="/public">public</Link>
+        </li>
+        <li>
+          <Link to="/protected">protected</Link>
+        </li>
+      </ul>
+      <Switch>
+        <Route path="/public">
+          <Public />
+        </Route>
+        <Route path="/protected">
+          <Protected />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
---
+++ client/src/components/Public.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Public = () => (
+  <h1>Public</h1>
+);
+
+export default Public;
